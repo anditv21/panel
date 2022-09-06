e9e2560dca6f8cec1b29207eac85a0195b649f16
anditv21 <75949923+anditv21@users.noreply.github.com>
2022-09-06 08:46:39 +0200

    ✅ Added pw reset

--- admin/includes/adminNavbar.inc.php
+++ admin/includes/adminNavbar.inc.php
@@ -7,6 +7,7 @@
 			<a href='index.php' class="btn btn-outline-primary btn-sm">Home</a>
 			<a href='users.php' class="btn btn-outline-primary btn-sm">Users</a>
 			<a href='bans.php' class="btn btn-outline-primary btn-sm">Bans</a>
+			<a href='password.php' class="btn btn-outline-primary btn-sm">Reset</a>
 			<a href='invites.php' class="btn btn-outline-primary btn-sm">Invite codes</a>
 			<a href='sub.php' class="btn btn-outline-primary btn-sm">Sub codes</a>
 			<a href='gift.php' class="btn btn-outline-primary btn-sm">Gift Sub</a>
