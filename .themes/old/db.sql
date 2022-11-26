985d619fcc42b30f4b729c4e997cf92cd533783c
Andreas <75949923+anditv21@users.noreply.github.com>
2022-11-26 19:03:03 +0100

    🔧 Updated db.sql

--- .themes/old/db.sql
+++ .themes/old/db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Aug 23, 2022 at 07:10 PM
--- Server version: 10.1.48-MariaDB-0+deb9u2
--- PHP Version: 7.4.29
+-- Host: localhost
+-- Generation Time: Oct 28, 2022 at 11:18 PM
+-- Server version: 10.3.36-MariaDB-0+deb10u2
+-- PHP Version: 8.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `panel`
+-- Database: `panel-edit`
 --
 
 -- --------------------------------------------------------
@@ -28,13 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cheat` (
-  `status` int(1) NOT NULL DEFAULT '0',
-  `version` float NOT NULL DEFAULT '0',
+  `status` int(1) NOT NULL DEFAULT 0,
+  `version` float NOT NULL DEFAULT 0,
   `news` varchar(255) NOT NULL DEFAULT '0',
-  `maintenance` int(1) NOT NULL DEFAULT '0',
-  `frozen` int(1) NOT NULL DEFAULT '0',
+  `maintenance` int(1) NOT NULL DEFAULT 0,
+  `frozen` int(1) NOT NULL DEFAULT 0,
   `freezingtime` int(13) NOT NULL,
-  `invites` int(1) NOT NULL DEFAULT '1'
+  `invites` int(1) NOT NULL DEFAULT 1
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `cheat` (
 --
 
 INSERT INTO `cheat` (`status`, `version`, `news`, `maintenance`, `frozen`, `freezingtime`, `invites`) VALUES
-(0, 1, 'Welcome to znixv2-panel-edit by anditv21!', 0, 1, 1661271058, 0);
+(0, 1, 'Welcome to znixv2-panel-edit by anditv21!', 0, 1, 1659443779, 0);
 
 -- --------------------------------------------------------
 
@@ -53,20 +53,17 @@ INSERT INTO `cheat` (`status`, `version`, `news`, `maintenance`, `frozen`, `free
 CREATE TABLE `invites` (
   `code` varchar(255) NOT NULL,
   `createdBy` varchar(255) NOT NULL,
-  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `createdAt` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `logs`
+-- Dumping data for table `invites`
 --
 
-CREATE TABLE `logs` (
-  `username` varchar(255) NOT NULL,
-  `date` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
-  `action` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `invites` (`code`, `createdBy`, `createdAt`) VALUES
+('7xWvD19OJoKUii8RbJ23', 'admin', '2022-10-01 17:33:54'),
+('2y56jmudDZKJowBWeZZ9', 'admin', '2022-10-01 18:50:15'),
+('ljJOJPArVSvCNw3KPUKl', 'admin', '2022-10-01 18:51:15');
 
 -- --------------------------------------------------------
 
@@ -79,7 +76,7 @@ CREATE TABLE `shoutbox` (
   `user` varchar(255) NOT NULL,
   `uid` int(255) NOT NULL,
   `msg` varchar(255) NOT NULL,
-  `time` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6)
+  `time` datetime(6) NOT NULL DEFAULT current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -98,8 +95,15 @@ INSERT INTO `shoutbox` (`id`, `user`, `uid`, `msg`, `time`) VALUES
 CREATE TABLE `subscription` (
   `code` varchar(255) NOT NULL,
   `createdBy` varchar(255) NOT NULL,
-  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `createdAt` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subscription`
+--
+
+INSERT INTO `subscription` (`code`, `createdBy`, `createdAt`) VALUES
+('3m-LHr4SVVAa3jY3KbnFeSG', 'admin', '2022-10-18 14:49:56');
 
 -- --------------------------------------------------------
 
@@ -112,14 +116,16 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `hwid` varchar(255) DEFAULT NULL,
-  `admin` int(1) NOT NULL DEFAULT '0',
-  `supp` int(1) NOT NULL DEFAULT '0',
+  `admin` int(1) NOT NULL DEFAULT 0,
+  `supp` int(1) NOT NULL DEFAULT 0,
   `sub` date DEFAULT NULL,
-  `frozen` int(1) NOT NULL,
-  `banned` int(1) NOT NULL DEFAULT '0',
+  `frozen` int(1) NOT NULL DEFAULT 0,
+  `banned` int(1) NOT NULL DEFAULT 0,
   `invitedBy` varchar(255) NOT NULL,
-  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `createdAt` timestamp NULL DEFAULT current_timestamp(),
   `lastIP` varchar(255) DEFAULT NULL,
+  `currentLogin` datetime DEFAULT NULL,
+  `lastLogin` datetime DEFAULT NULL,
   `remembertoken` varchar(50) DEFAULT NULL,
   `banreason` varchar(255) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -128,24 +134,24 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`uid`, `username`, `password`, `hwid`, `admin`, `supp`, `sub`, `frozen`, `banned`, `invitedBy`, `createdAt`, `lastIP`, `remembertoken`, `banreason`) VALUES
-(1, 'admin', '$2y$10$7wOzYc.AXpXc1nE/b0IqLOsP2w1cK9LZXDUi6hoSyuWBDj3DoBjOK', 'hwid', 1, 1, '2088-05-08', 1, 0, '', '2022-07-05 22:04:37', '', '', NULL);
+INSERT INTO `users` (`uid`, `username`, `password`, `hwid`, `admin`, `supp`, `sub`, `frozen`, `banned`, `invitedBy`, `createdAt`, `lastIP`, `currentLogin`, `lastLogin`, `remembertoken`, `banreason`) VALUES
+(1, 'admin', '$2y$10$7wOzYc.AXpXc1nE/b0IqLOsP2w1cK9LZXDUi6hoSyuWBDj3DoBjOK', 'hwid', 1, 1, '2154-03-03', 1, 0, '', '2022-07-05 22:04:37', NULL, '2022-10-18 15:14:47', '2022-10-18 13:12:35', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `cheat`
+--
+ALTER TABLE `cheat`
+  ADD PRIMARY KEY (`freezingtime`);
+
+--
 -- Indexes for table `invites`
 --
 ALTER TABLE `invites`
   ADD UNIQUE KEY `code` (`code`);
-
---
--- Indexes for table `logs`
---
-ALTER TABLE `logs`
-  ADD UNIQUE KEY `date` (`date`);
 
 --
 -- Indexes for table `shoutbox`
