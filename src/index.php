3a1aeca4fb44ca944c30f53ccc21a556bb66edac
Andreas <anditv21@icloud.com>
2022-12-29 19:30:54 +0100

    🔧 Some changes

--- src/index.php
+++ src/index.php
@@ -217,12 +217,6 @@
       </div>
       </div>
       </div>
-      <style>
-         img
-         {
-         margin-bottom: 1%;
-         }
-      </style>
       <script src="assets/bootstrap/js/bootstrap.min.js"></script>
       <script src="assets/js/bs-init.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
