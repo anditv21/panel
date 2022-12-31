7f4a23756ef4c7c5d2298b99a9b809e046906d66
Andreas <anditv21@icloud.com>
2022-12-31 11:49:40 +0100

    🔧 Improved IP check

--- src/app/models/UsersModel.php
+++ src/app/models/UsersModel.php
@@ -494,20 +494,16 @@ class Users extends Database
 
     public function getip()
     {
-        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-            $_SERVER["REMOTE_ADDR"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-            $_SERVER["HTTP_CLIENT_IP"] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-        }
-        $client = @$_SERVER["HTTP_CLIENT_IP"];
-        $forward = @$_SERVER["HTTP_X_FORWARDED_FOR"];
-        $remote = $_SERVER["REMOTE_ADDR"];
-
-        if (filter_var($client, FILTER_VALIDATE_IP)) {
-            $ip = $client;
-        } elseif (filter_var($forward, FILTER_VALIDATE_IP)) {
-            $ip = $forward;
+        $clientIp = filter_var($_SERVER["HTTP_CLIENT_IP"] ?? '', FILTER_VALIDATE_IP);
+        $forwardedIp = filter_var($_SERVER["HTTP_X_FORWARDED_FOR"] ?? '', FILTER_VALIDATE_IP);
+        $remoteIp = filter_var($_SERVER["REMOTE_ADDR"], FILTER_VALIDATE_IP);
+    
+        if ($clientIp) {
+            $ip = $clientIp;
+        } elseif ($forwardedIp) {
+            $ip = $forwardedIp;
         } else {
-            $ip = $remote;
+            $ip = $remoteIp;
         }
         return $ip;
     }
