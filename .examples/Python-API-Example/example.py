52e6d39fd336d692598a866bb29f35260e0e62fa
Andreas <anditv21@icloud.com>
2023-01-12 02:03:27 +0100

    🔧 Updated Python Example

--- .examples/Python-API-Example/example.py
+++ .examples/Python-API-Example/example.py
@@ -1,15 +1,61 @@
-import os
+import sys
 import subprocess
 import base64
 import requests
 import json
 import datetime
+import time
+from getpass import getpass
 
 def main():
     username = input(f'[Login] Username >> ')
-    password = input(f'[Login] Password >> ')    
-    print(send_login_request(username, password, hwid))
+    password = getpass(f'[Login] Password >> ') 
+    apiresult = json.loads(str(send_login_request(username, password, hwid)))
     
+    
+    if (apiresult["status"] == "failed"):
+        print("Username or password incorrect.")
+        time.sleep(500)
+        sys.exit()
+        
+        
+    # version check
+    if(str(apiresult["cheatversion"]) != VERSION):
+        print("You are using a outdated version.")
+        print(apiresult["cheatversion"]) 
+        time.sleep(500)
+        sys.exit()
+            
+        
+    # print cheat status
+    if(apiresult["cheatstatus"] == "0"):
+        print("Status: Undetected")
+    elif(apiresult["cheatstatus"] == "1"):
+        print("Status: Detected")
+    elif(apiresult["cheatmaintenance"] == "1"):
+        print("Status: Maintenance")    
+        
+    # ban check
+    if(apiresult["banned"] == "1"):
+        print("You have been banned.")
+        time.sleep(500)
+        sys.exit()
+        
+    
+    print(f"You have {checksub(apiresult['sub'])} day/s sub left.")           
+    
+    
+        # hwid check
+    if(get_hardware_id() == apiresult["hwid"]):
+        print("HIWD does match.")
+    elif(apiresult["hwid"] == None or ""):
+        print("HIWD does match.")
+    else:
+        print("HWID does not match.")      
+        time.sleep(500)
+        sys.exit()  
+        
+                
 def get_config():
     # Read the configuration file
     with open('config.json', 'r') as f:
@@ -40,24 +86,35 @@ def send_login_request(username, password, hwid):
 
 
     # Send the login request
-    r = requests.get(f'https://{DOMAIN}/api.php?user={username}&pass={password}&hwid={hwid}&key={API_KEY}')
-    return r.text
+    apiresult = requests.get(f'https://{DOMAIN}/api.php?user={username}&pass={password}&hwid={hwid}&key={API_KEY}')
+    return apiresult.text
 
 def check_subscription_expired(subscription_date):
     curr_date = datetime.datetime.now()
     sub_date = datetime.datetime.strptime(subscription_date, '%Y-%m-%d')
     return curr_date >= sub_date
 
+def checksub(sub):
+    if not sub:
+        return 0
+    else:
+        value = (datetime.datetime.strptime(sub, '%Y-%m-%d') - datetime.datetime.now()).days
+        return value
+    
+    
+    
 # Constants
 DOMAIN = 'anditv.it/panel' 
 API_KEY = 'yes' # api key from config file on panel
+VERSION = '1'
 
 # Get the hardware id
 hwid = get_hardware_id()
 if hwid is None:
     # Log the error and exit
     print("HWID not found.")
-    os._exit(0)
+    time.sleep(500)
+    sys.exit()
 
 if __name__ == "__main__":
     main()
