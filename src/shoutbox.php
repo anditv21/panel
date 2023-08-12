3bea4e2509d2fc32450a581afccb7359bab992f5
Andreas <andi@anditv.dev>
2023-08-12 19:58:40 +0200

    ✅ Added default avatar to shoutbox

--- src/shoutbox.php
+++ src/shoutbox.php
@@ -22,38 +22,39 @@ foreach ($messages as $message) :
   }
 ?>
 
-<div class="d-flex">
-  <div>
-  <a href="<?php Util::getavatardl($message['uid']); ?>" download="<?php Util::display($message['uid'] . Util::getextention($message['uid'])); ?>">
-        <img src="<?php Util::display(Util::getavatardl($message['uid'])); ?>" class="rounded-circle img-profile" width="45" height="45">
+  <div class="d-flex">
+    <div>
+      <a href="<?php echo Util::getavatardl($message['uid']) ?: '/assets/img/avatars/Portrait_Placeholder.png'; ?>" download="<?php echo Util::display($message['uid'] . Util::getextention($message['uid'])); ?>">
+        <img src="<?php Util::display(Util::getavatardl($message['uid']) ?: SUB_DIR. '/assets/img/avatars/Portrait_Placeholder.png'); ?>" class="rounded-circle img-profile" width="45" height="45">
       </a>
-    <strong>
-      <?php
-      if ($message["uid"] == Session::get("uid")) {
-        Util::display('<span class="own-username username">');
-      } elseif ($user_role == "admin") {
-        Util::display('<span class="admin-username username">');
-      } elseif ($user_role == "supp") {
-        Util::display('<span class="supp-username username">');
-      }
-      ?>
-      <a href="<?php Util::display(SUB_DIR. "/viewprofile.php?uid=".$message['uid']); ?>" >
+
+      <strong>
         <?php
-        $userbyid = $user->getuserbyuid($message['uid']);
-        $displayname = $user->fetch_display_name($userbyid->username);
-        Util::display(Util::securevar($displayname . " ($message[uid]) @ " . $message["time"])); ?>
-      </a>
+        if ($message["uid"] == Session::get("uid")) {
+          Util::display('<span class="own-username username">');
+        } elseif ($user_role == "admin") {
+          Util::display('<span class="admin-username username">');
+        } elseif ($user_role == "supp") {
+          Util::display('<span class="supp-username username">');
+        }
+        ?>
+        <a href="<?php Util::display(SUB_DIR . "/viewprofile.php?uid=" . $message['uid']); ?>">
+          <?php
+          $userbyid = $user->getuserbyuid($message['uid']);
+          $displayname = $user->fetch_display_name($userbyid->username);
+          Util::display(Util::securevar($displayname . " ($message[uid]) @ " . $message["time"])); ?>
+        </a>
+        <?php
+        if ($user_role != "" || $message["uid"] == Session::get("uid")) {
+          Util::display('</span>');
+        }
+        ?>:
+      </strong>
       <?php
-      if ($user_role != "" || $message["uid"] == Session::get("uid")) {
-        Util::display('</span>');
-      }
-      ?>:
-    </strong>
-    <?php
-    Util::display(Util::securevar($message["message"]));
-    ?>
+      Util::display(Util::securevar($message["message"]));
+      ?>
+    </div>
   </div>
-</div>
 
 <?php endforeach; ?>
 
