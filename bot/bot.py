ec9e460538c1a909dae33104979689178d4b8487
Andreas <andi@anditv.dev>
2023-08-14 21:55:40 +0200

    ✅ Added basic bot template

---
+++ bot/bot.py
@@ -0,0 +1,86 @@
+import asyncio
+import os
+import platform
+import sys
+from datetime import datetime
+from urllib.parse import quote
+
+import discord
+from colorama import Fore
+from discord.ext import commands, tasks
+
+from helpers.config import get_config_value
+from helpers.general import (clear_console)
+
+sys.dont_write_bytecode = True
+
+
+token = get_config_value("token")
+
+class Bot(commands.Bot):
+    def __init__(self, *, intents: discord.Intents):
+
+        super().__init__(command_prefix=commands.when_mentioned_or("$$"), intents=intents)
+
+    async def setup_hook(self):
+        clear_console()
+        print("Loading cogs...")
+        for filepath in os.listdir('cogs'):
+            for filename in os.listdir(f'cogs/{filepath}'):
+                if filename.endswith('.py'):
+                    filename = filename.replace('.py', '')
+                    try:
+                        await bot.load_extension(f'cogs.{filepath}.{filename}')
+                    except Exception as error:
+                        print(f'Failed to load cogs.{filepath}.{filename}: {error}')
+
+        await self.tree.sync()
+
+
+intents = discord.Intents.all()
+intents.presences = True
+intents.members = True
+bot = Bot(intents=intents)
+bot.remove_command("help")
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.idle)
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="anditv.dev"),)
+    print(f'[BOT] has connected as {bot.user} via discord.py {discord.__version__}')
+
+    bg_task.start()
+
+
+
+
+@tasks.loop(seconds=5)
+async def bg_task():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+
+
+        status_list = [
+            (discord.Status.dnd, discord.Activity(
+                type=discord.ActivityType.watching, name="github.com/anditv21/panel")),
+            (discord.Status.dnd, discord.Activity(
+                type=discord.ActivityType.watching, name="anditv.dev")),
+            (discord.Status.dnd, discord.Activity(
+                type=discord.ActivityType.watching, name=f"{len(bot.guilds)} servers"))
+        ]
+
+        if platform.system() == 'Windows':
+            status_list[2] = (discord.Status.idle, discord.Activity(
+                type=discord.ActivityType.playing, name="development"))
+            await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(
+                type=discord.ActivityType.playing, name="development"))
+            return False
+
+"""
+@bot.event
+async def on_message(message):
+    print(str(message.content))
+"""
+
+bot.run(token=token, log_level=40)
---
+++ bot/helpers/config.py
@@ -0,0 +1,25 @@
+import json
+import os
+import sys
+from datetime import datetime
+
+sys.dont_write_bytecode = True
+time = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+
+def get_config_value(key: str) -> str:
+    config_path = "config.json"
+    if not os.path.isfile(config_path):
+        parent_dir = os.path.dirname(os.path.abspath(__file__))
+        config_path = os.path.join(parent_dir, "..", "config.json")
+
+        if not os.path.isfile(config_path):
+            print("[ERROR] config.json not found in the current directory or its parent directory. Please make sure the file exists.")
+            sys.exit()
+
+    with open(config_path, "r", encoding="UTF-8") as configfile:
+        config = json.load(configfile)
+        value = config.get(key)
+        if not value:
+            print(f"[ERROR] Value for key '{key}' is missing from config.json. Please check the configuration file and try again.")
+            sys.exit()
+    return value
