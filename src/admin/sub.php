27f8016f61596fb7dab975d39090484a92d5f313
Andreas <andi@anditv.dev>
2023-10-27 16:26:26 +0200

    🗑 Removed broken bootstrap includes

--- src/admin/sub.php
+++ src/admin/sub.php
@@ -65,7 +65,6 @@ if (Util::securevar($_SERVER['REQUEST_METHOD']) === 'POST') {
 <link rel="stylesheet" href="../assets/css/custom.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
 <div class="divide"></div>
 
 <div class="container mt-2">
--- src/user/log.php
+++ src/user/log.php
@@ -30,7 +30,6 @@ if (Util::securevar($_SERVER["REQUEST_METHOD"]) === "POST") {
 ?>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="../assets/css/custom.css">
 <style>
    .modal-content {
