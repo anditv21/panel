658feb6216b2540d025e2f9b65062f39915a6316
Andreas <andi@anditv.dev>
2026-07-18 23:06:41 +0200

    🐛 Fixed captcha validation

--- src/app/controllers/SystemController.php
+++ src/app/controllers/SystemController.php
@@ -28,61 +28,100 @@ class SystemController extends System
         return $this->getCaptchaService();
     }
 
-    public function vaildateCaptcha()
+    public function vaildateCaptcha($data = [])
     {
         $captcha_service = $this->getCapService();
 
         $secret = $this->getSecret();
         if ($captcha_service == 1) {
+            $captcha_response = isset($data['cf-turnstile-response']) ? $data['cf-turnstile-response'] : '';
+
+            if (empty($captcha_response)) {
+                return false;
+            }
+
             $hdata = array(
                 'secret' => $secret,
-                'response' =>  Util::securevar($_POST['cf-turnstile-response'])
+                'response' => $captcha_response
             );
             $verify = curl_init();
             curl_setopt($verify, CURLOPT_URL, "https://challenges.cloudflare.com/turnstile/v0/siteverify");
             curl_setopt($verify, CURLOPT_POST, true);
             curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($hdata));
             curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($verify, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($verify, CURLOPT_TIMEOUT, 10);
             $response = curl_exec($verify);
-            // var_dump($response);
+            curl_close($verify);
+
+            if ($response === false) {
+                return false;
+            }
+
             $responseData = json_decode($response);
-            if ($responseData->success) {
+            if (is_object($responseData) && !empty($responseData->success)) {
                 return true;
             } else {
                 return false;
             }
         } elseif($captcha_service == 2) {
+            $captcha_response = isset($data['h-captcha-response']) ? $data['h-captcha-response'] : '';
+
+            if (empty($captcha_response)) {
+                return false;
+            }
+
             $hdata = array(
                 'secret' => $secret,
-                'response' =>  Util::securevar($_POST['h-captcha-response'])
+                'response' => $captcha_response
             );
             $verify = curl_init();
             curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
             curl_setopt($verify, CURLOPT_POST, true);
             curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($hdata));
             curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($verify, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($verify, CURLOPT_TIMEOUT, 10);
             $response = curl_exec($verify);
-            // var_dump($response);
+            curl_close($verify);
+
+            if ($response === false) {
+                return false;
+            }
+
             $responseData = json_decode($response);
-            if ($responseData->success) {
+            if (is_object($responseData) && !empty($responseData->success)) {
                 return true;
             } else {
                 return false;
             }
         } elseif($captcha_service == 3) {
+            $captcha_response = isset($data['g-recaptcha-response']) ? $data['g-recaptcha-response'] : '';
+
+            if (empty($captcha_response)) {
+                return false;
+            }
+
             $hdata = array(
                 'secret' => $secret,
-                'response' => Util::securevar($_POST['g-recaptcha-response'])
+                'response' => $captcha_response
             );
             $verify = curl_init();
             curl_setopt($verify, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
             curl_setopt($verify, CURLOPT_POST, true);
             curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($hdata));
             curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($verify, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($verify, CURLOPT_TIMEOUT, 10);
             $response = curl_exec($verify);
-            // var_dump($response);
+            curl_close($verify);
+
+            if ($response === false) {
+                return false;
+            }
+
             $responseData = json_decode($response);
-            if ($responseData->success) {
+            if (is_object($responseData) && !empty($responseData->success)) {
                 return true;
             } else {
                 return false;
@@ -90,5 +129,7 @@ class SystemController extends System
         } elseif($captcha_service == 0) {
             return true;
         }
+
+        return false;
     }
 }
